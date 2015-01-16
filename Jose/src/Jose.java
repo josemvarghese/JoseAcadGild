c05099b3713eea584d5c7e012a2755811efb1f31
josemvarghese <josemvarghesee@gmail.com>
2015-01-16 23:15:38 +0530

    Jose

---
+++ Jose/src/Jose.java
@@ -0,0 +1,14 @@
+
+public class Jose {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jose");
+		int num=23;
+		System.out.println((double)num);
+		System.out.println("josewww");
+	}
+}
